354bb7b29fc57dd3beb3fbd4b5c8c59f55d9910d
joshua-kfbb <iyezi@hotmail.com>
2017-04-12 22:43:42 +0100

    yooooooooooo

--- WebContent/js/gameRowView.js
+++
@@ -1,9 +0,0 @@
-var GameRowView=Backbone.View.extend({
-    model:Game,
-    tagName:'tr',
-    events:{},
-    render:function () {
-        var template=_.template($('#gamebiao').html());
-        return this.$el.html(template(this.model.toJSON()));
-    }
-});
--- WebContent/js/gameTableView.js
+++ WebContent/js/gameTableView.js
@@ -25,3 +25,12 @@ var GameTableView = Backbone.View.extend({
         $('#game-table').DataTable();
     }
 });
+var GameRowView=Backbone.View.extend({
+    model:Game,
+    tagName:'tr',
+    events:{},
+    render:function () {
+        var template=_.template($('#gamebiao').html());
+        return this.$el.html(template(this.model.toJSON()));
+    }
+});
